8df4e92b54015ee8a8dd4d9e2df087bd84862d74
marcosdgomes <marcos-dellano@hotmail.com>
2021-02-20 21:45:29 -0500

    v1.1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to online development"/>
+  <div id="app">
+    <h1>Conversor de Moedas</h1>
+    <h3>Principais Moedas Estrangeiras x Real (BRL)</h3>
+    <div class="linha">
+    <conversor moeda-a="USD" moeda-b="BRL"/>
+    <conversor moeda-a="CAD" moeda-b="BRL"/>
+  </div>
+  <div class="linha">
+    <conversor moeda-a="EUR" moeda-b="BRL"/>
+    <conversor moeda-a="GBP" moeda-b="BRL"/>
+    
+    </div>
+    <h3>Real  (BRL) x Moedas estrangeiras</h3>
+    <div class="linha">
+    <conversor moeda-a="BRL" moeda-b="USD"/>
+    <conversor moeda-a="BRL" moeda-b="EUR"/>
+    <conversor moeda-a="BRL" moeda-b="GBP"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import conversor from "./components/conversor";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    conversor
   }
-}
+};
 </script>
 
 <style>
 #app {
+  margin: auto;
+  max-width: 900px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.linha{
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-between;
 }
 </style>
